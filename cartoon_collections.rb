75281e45d7c07298d93598dae49f223610259b6f
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-09 22:22:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,5 @@
 def roll_call_dwarves(list)
-  list.each_with_index do |dwarf,index|
-    puts "#{index+1}. #{dwarf}"
-  end
+  list.each_with_index {|dwarf,index| puts "#{index+1}. #{dwarf}"}
 end
 
 def summon_captain_planet(list)
